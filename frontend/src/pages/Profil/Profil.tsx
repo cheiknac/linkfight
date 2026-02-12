4f06e3b7c21ea43858976d79e2e4b772e2e5f96c
cheiknac <cheikna.coulibaly@hotmail.fr>
2026-02-12 01:50:09 +0100

    add information profil

--- frontend/src/pages/Profil/Profil.tsx
+++ frontend/src/pages/Profil/Profil.tsx
@@ -21,6 +21,35 @@ export default function Profil() {
                     </div>
                 </div>
 
+                <div className="blocContainer">
+                    <h1>Rayan MAZUCHI</h1>
+                    <p><strong>Age : </strong>14 ans</p>
+                    <p><strong>Catégorie : </strong>14 ans</p>
+                    <p><strong>Discipline : </strong>14 ans</p>
+                    <p><strong>Club : </strong>14 ans</p>
+                </div>
+
+                <div className="blocContainer specialStyle">
+                    <h4>Victoire</h4>
+                    <p className="numberText">15</p>
+                </div>
+
+                <div className="blocContainer">
+                    <h4>Défaite</h4>
+                    <p className="numberText">0</p>
+                </div>
+
+                <div className="blocContainer">
+                    <h4>Biographie</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                        sed do eiusmod tempor incididunt ut labore et dolore magna 
+                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+                        ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                        Duis aute irure dolor in reprehenderit in voluptate velit 
+                        esse cillum dolore eu fugiat nulla pariatur.
+                    </p>
+                </div>
+
 
             </div>
         </div>
