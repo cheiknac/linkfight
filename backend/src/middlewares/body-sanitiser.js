851b6be869907b48419d5e5ba9b0ea3f0ce39cbd
cheicknac <cheikna.coulibaly@hotamil.fr>
2026-08-18 00:56:31 +0200

    add body sanitiser html middleware

---
+++ backend/src/middlewares/body-sanitiser.js
@@ -0,0 +1,17 @@
+import sanitizeHtml from 'sanitize-html';
+
+export const bodySanitiser = (req, res, next) => {
+    const body = req.body;
+
+    for (const key in body) {
+        if (typeof body[key] === 'string') {
+            body[key] = sanitizeHtml(body[key], {
+                allowedTags: [],
+                allowedAttributes: {},
+            });
+        }
+    }
+
+    next();
+}
+
