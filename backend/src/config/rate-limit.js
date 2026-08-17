8c7e19f1f887705f21eba71e78316f4b078b23f6
cheicknac <cheikna.coulibaly@hotamil.fr>
2026-08-18 01:03:54 +0200

    add rate limit config

---
+++ backend/src/config/rate-limit.js
@@ -0,0 +1,13 @@
+import { rateLimit } from 'express-rate-limit';
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+  standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+	// store: ... , // Redis, Memcached, etc. See below.
+  message: 'vous avez dépassé le nombre de requêtes autorisées. Veuillez réessayer plus tard.',
+});
+
+app.use(limiter)
